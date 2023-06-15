a1dc7e2a4ac1159064ddefdce9206614201f400f
ProgrammingGoldflash <philip.ainberger@aon.at>
2023-06-15 19:16:24 +0200

    add both icon containers

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Src/**/*.{html,js}"],
+  content: [
+    "./Src/index.html",
+    "./Src/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
