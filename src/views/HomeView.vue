5f1bbac4078d1551010eda29f6d960794bb3e4f0
ProgrammingGoldflash <philip.ainberger@aon.at>
2023-07-20 19:49:48 +0200

    adapt icon listing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@ library.add(faCodeBranch, faWrench, faNewspaper, faAnglesDown, faComputerMouse)
   <div class="absolute inset-0 z-0 bg-[url(../assets/grid.svg)] bg-center"></div>
 
   <main class="flex flex-col md:flex-row flex-grow h-screen relative">
-    <div class="md:w-1/2 p-4 grid bg-black bg-opacity-5 justify-end content-center">
+    <div class="md:w-1/2 p-4 grid justify-end content-center">
       <div class="grid justify-end content-center text-center space-y-3">
         <h2 class="text-xl font-bold uppercase z-1">Philip</h2>
         <p class="">
@@ -19,15 +19,15 @@ library.add(faCodeBranch, faWrench, faNewspaper, faAnglesDown, faComputerMouse)
           person.
         </p>
         <!-- <a href="#example">Skills</a> -->
-        <a href="#example" class="bg-orange-600 text-white font-semibold p-1 rounded-md">
+        <a href="#techstack" class="bg-orange-600 text-white font-semibold p-1 rounded-md">
           <font-awesome-icon icon="fa-solid fa-code-branch" />
           techstack
         </a>
-        <a href="#example" class="bg-orange-500 text-white font-semibold p-1 rounded-md">
+        <a href="#projects-articles" class="bg-orange-500 text-white font-semibold p-1 rounded-md">
           <font-awesome-icon icon="fa-solid fa-wrench" />
           projects
         </a>
-        <a href="#example" class="bg-orange-400 text-white font-semibold p-1 rounded-md">
+        <a href="#projects-articles" class="bg-orange-400 text-white font-semibold p-1 rounded-md">
           <font-awesome-icon icon="fa-solid fa-newspaper" />
           articles
         </a>
@@ -46,35 +46,39 @@ library.add(faCodeBranch, faWrench, faNewspaper, faAnglesDown, faComputerMouse)
     </div>
   </main>
 
-  <div class="flex flex-col md:flex-row flex-grow h-screen relative h-screen">
+  <div id="techstack" class="flex flex-col md:flex-row flex-grow relative h-screen">
     <div class="md:w-1/2 p-4 flex flex-col">
-      <div class="text-center w-full font-bold uppercase my-8 text-xl">
-        Tools and technologies I use on a daily basis
+      <div class="horizontal-line-container">
+        <div class="horizontal-line"></div>
+        <h2 class="title">Tools and technologies I use on a daily basis</h2>
+        <div class="horizontal-line"></div>
       </div>
       <div class="container" ref="techStack" style="height: 100%;">
-        <div v-for="(tech, i) in myTechStack" :key="i"
-          :style="{ left: `${tech.positionX}px`, top: `${tech.positionY}px`, animation: `float${i} ${tech.duration}s ease-in-out infinite` }"
-          class="icon">
+        <div v-for="(tech, i) in myTechStack" :key="i" class="icon">
           <img :src="`./src/assets/images/techStack/${tech.icon}`" />
+          <p class="icon-caption">{{ tech.icon.split('.').slice(0, -1).join('.') }}</p>
         </div>
       </div>
+
     </div>
-    <div class="md:w-1/2 p-4 flex flex-col bg-black bg-opacity-10 justify-end content-center">
-      <div class="text-center w-full font-bold uppercase my-8 text-xl">
-        Technologies that I used in projects
+    <div class="md:w-1/2 p-4 flex flex-col justify-end content-center">
+      <div class="horizontal-line-container">
+        <div class="horizontal-line"></div>
+        <h2 class="title">Technologies that I used in projects</h2>
+        <div class="horizontal-line"></div>
       </div>
       <div class="container" ref="experience" style="height: 100%;">
-        <div v-for="(exp, i) in experience" :key="i"
-          :style="{ left: `${exp.positionX}px`, top: `${exp.positionY}px`, animation: `float${i + myTechStack.length} ${exp.duration}s ease-in-out infinite` }"
-          class="icon">
+        <div v-for="(exp, i) in experience" :key="i" class="icon">
           <img :src="`./src/assets/images/experience/${exp.icon}`" />
+          <p class="icon-caption">{{ exp.icon.split('.').slice(0, -1).join('.') }}</p>
         </div>
       </div>
+
     </div>
   </div>
 
-  <div class="flex flex-col md:flex-row flex-grow h-screen relative h-screen">
-    <div class="md:w-1/2 p-4 grid bg-black bg-opacity-10 justify-end content-center">
+  <div id="projects-articles" class="flex flex-col md:flex-row flex-grow relative h-screen">
+    <div class="md:w-1/2 p-4 grid justify-end content-center">
 
     </div>
 
@@ -95,14 +99,13 @@ export default {
   },
   mounted() {
     this.myTechStack = this.initIcons(
-      ["azure.png", "csharp.png", "dotnet.png", "javascript.png", "ms_sql_server.png", "visual_studio.png", "visual_studio_code.png", "blazor.png", "postman.png", "git.png", "html.png", "css.png", "webassembly.png", "docker.png"],
+      this.shuffleArray(["Azure.png", "CSharp.png", "dotNET.png", "Javascript.png", "SQLServer.png", "Visual Studio.png", "Visual Studio Code.png", "Blazor.png", "Postman.png", "Git.png", "Html.png", "Css.png", "WebAssembly.png", "Docker.png"]),
       this.$refs.techStack as HTMLElement
     );
     this.experience = this.initIcons(
-      ["angular.png", "cplusplus.png", "firebase.png", "grafana.png", "graphql.png", "java.png", "mysql.png", "next_js.png", "node_js.png", "open_api.png", "php.png", "react.png", "redux.png", "typescript.png", "nginx.png", "mongo_db.png"],
+      this.shuffleArray(["Angular.png", "C++.png", "Firebase.png", "Grafana.png", "GraphQL.png", "Java.png", "MySQL.png", "NextJS.png", "NodeJS.png", "OpenApi.png", "Php.png", "React.png", "Redux.png", "Typescript.png", "Nginx.png", "MongoDB.png"]),
       this.$refs.experience as HTMLElement
     );
-    this.appendStyles();
   },
   methods: {
     halton(n: number, base: number) {
@@ -127,39 +130,77 @@ export default {
         return new TechStackIcon(icon, x, y, duration, distanceX, distanceY);
       });
     },
-    appendStyles() {
-      let styleElement = document.createElement('style');
-      [...this.myTechStack, ...this.experience].forEach((item, i) => {
-        let keyframes = `
-          @keyframes float${i} {
-            0%, 100% {
-              transform: translate(0, 0);
-            }
-            50% {
-              transform: translate(${item.distanceX}px, ${item.distanceY}px);
-            }
-          }
-        `;
-        styleElement.textContent += keyframes;
-      });
-      document.head.appendChild(styleElement);
-    },
+    shuffleArray(array: Array<string>) {
+      let currentIndex = array.length, temporaryValue, randomIndex;
+
+      while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
   },
 };
 </script>
 
 <style>
+.horizontal-line-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  margin-left: 4rem;
+  margin-right: 4rem;
+  /* adjust as needed */
+}
+
+.title {
+  font-style: italic;
+  font-size: 1.5rem;
+  margin: 0 20px;
+  z-index: 1;
+}
+
+.horizontal-line {
+  flex-grow: 1;
+  height: 2px;
+  background-color: #a5a5a5;
+  /* adjust color as needed */
+  z-index: 0;
+}
+
 .icon {
   width: 80px;
   height: 90px;
   margin: 10px;
-  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
 
 .container {
   position: relative;
   height: 50%;
-  padding: 30px;
+  padding: 5vh;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  justify-items: center;
+  align-items: center;
+}
+
+.icon-caption {
+  margin-top: 5px;
+  font-size: 0.6rem;
+  font-weight: bold;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-style: italic;
 }
 
 .right-column {
